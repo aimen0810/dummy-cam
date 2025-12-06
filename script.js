8b524d4f7d3fd3b5ef101214f77a651654448be0
aimen0810 <164145319+aimen0810@users.noreply.github.com>
2025-12-06 22:26:33 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === "aimen" && pass === "aimen") {
+        // Redirect to second page
+        window.location.href = "page2.html";
+    } else {
+        alert("Incorrect username or password");
+    }
+}
